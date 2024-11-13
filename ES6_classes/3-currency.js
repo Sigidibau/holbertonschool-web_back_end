7686a529959b065466717d1631dc56a87ba1b953
sigidibau <9571@holbertonstudents.com>
2024-11-13 11:52:43 -0400

    mehtods

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,34 +1,35 @@
-export default class Currency {
+class Currency {
   constructor(code, name) {
     this.code = code;
     this.name = name;
-}
+  }
 
-get code() {
-  return this._code;
-}
+  get code() {
+    return this._code;
+  }
 
-set code(code) {
-  if (typeof code !== 'string') {
-    throw new TypeError('Code must be a string');
-  }   else {
-  this._code = code;
-}
-}
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
 
-get name() {
-  return this._name;
-}
+  get name() {
+    return this._name;
+  }
 
-set name(name) {
-  if (typeof name !== 'string') {
-    throw new TypeError('Name must be a string');
-}   else {
-    this._name = name;
-}
-}
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
 
-displayFullCurrency() {
-  return `${this.name} (${this.code})`;
+  displayFullCurrency() {
+    return `${this._name} (${this.code})`;
+  }
 }
-}
+export default Currency;
