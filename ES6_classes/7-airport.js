4ddb742cc7dfae306506ae3ff7d1303a593234f2
sigidibau <9571@holbertonstudents.com>
2024-11-13 11:58:56 -0400

    same

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,16 @@
+class Airport {
+    constructor(name, code) {
+      this._name = name;
+      this._code = code;
+    }
+  
+    toString() {
+      return `[object ${this._code}]`;
+    }
+  
+    get [Symbol.toStringTag]() {
+      return this._code;
+    }
+  }
+  
+  export default Airport;
