dd5ae6e4501b1e0110dfba83a9a2dafebf18003f
sigidibau <9571@holbertonstudents.com>
2024-11-19 09:16:02 -0400

    fixes

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,7 @@
 export default function cleanSet(set, startString) {
-  // Filter the set to find values that start with the startString
   const filteredValues = [...set]
     .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length)); // Remove startString from each matching value
+    .map((value) => value.slice(startString.length));
 
-  // Join the filtered values into a single string separated by hyphens
   return filteredValues.join('-');
 }
