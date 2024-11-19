2c7c7c0586bac3ed8cd0c8794af7047e3c97ec72
sigidibau <9571@holbertonstudents.com>
2024-11-19 08:24:44 -0400

    geting the id of a list

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,7 @@
 export default function getListStudentIds(students) {
-  // Check if the input is an array
-  if (!Array.isArray(students)) {
-    return [];  // Return an empty array if input is not an array
-  }
-  
-  // Use map to extract the ids
-  return students.map(student => student.id);
+if (!Array.isArray(students)) {
+  return [];
+}
+
+return students.map(student => student.id);
 }
