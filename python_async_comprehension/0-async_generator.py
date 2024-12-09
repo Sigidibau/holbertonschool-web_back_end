e18e065cc653971b92db1b1cf6a624884543adee
sigidibau <9571@holbertonstudents.com>
2024-12-09 11:45:34 -0400

    fix wbhite spaces

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+"""imports for my asyncio"""
 
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
-
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Asynchronous generator that yields a random integer between 0 and 10,
     10 times with a 1-second asynchronous delay between each yield.
@@ -17,5 +18,5 @@ async def async_generator():
         int: A random integer between 0 and 10.
     """
     for _ in range(10):
-        await asyncio.sleep(1)
         yield random.randit(0, 10)
+        await asyncio.sleep(1)
