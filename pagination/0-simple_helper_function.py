101e4eb7a79c12a74cd9fe1d4afe90a731a13c5e
sigidibau <9571@holbertonstudents.com>
2024-12-10 11:19:34 -0400

    fisrt task

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -14,4 +14,4 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
         page (int): page number
         page_size (int): size of the page
     """
-    return((page - 1) * page_size, page * page_size)
+    return ((page - 1) * page_size, page * page_size)
